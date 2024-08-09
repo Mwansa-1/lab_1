9895df0d07a109c1d331da2af686a3ad5af93786
Mwansa-1 <mwelwamwansa18@gmail.com>
2024-08-09 14:32:31 +0200

    change to settigns

--- lab_1/settings.py
+++ lab_1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4x%g221l3p4_a+_z5e9ycvfsv!%7w!a(-@_9(jv#a)qe)*n1)6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
